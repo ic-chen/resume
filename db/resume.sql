7fcaf5da9e2269c228a3fd04920dc0ddb7d5038d
ic-chen <mimi2344@hotmail.com>
2019-12-09 16:18:24 +0800

    2019-12-09 (修改db)

--- db/resume.sql
+++ db/resume.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2019-12-04 16:36:23
+-- 產生時間： 2019-12-09 16:17:01
 -- 伺服器版本： 10.4.6-MariaDB
 -- PHP 版本： 7.3.9
 
@@ -29,14 +29,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `edu` (
-  `id` int(20) NOT NULL,
+  `id` int(11) NOT NULL,
   `acct` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '帳號',
-  `see` tinyint(1) NOT NULL DEFAULT 1 COMMENT '可見',
+  `see` tinyint(1) NOT NULL DEFAULT 0 COMMENT '可見',
   `grad_t` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '就讀時間',
+  `grad_st` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT 'NULL' COMMENT '畢業狀態',
   `sch` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '學校',
-  `dept` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '科系',
-  `grad_st` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '畢業狀態'
+  `dept` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '科系'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `edu`
+--
+
+INSERT INTO `edu` (`id`, `acct`, `see`, `grad_t`, `grad_st`, `sch`, `dept`) VALUES
+(1, 'ic123456', 0, '22222222222', '畢業', '22222222222', '22222222222'),
+(4, 'ic123456', 0, '999999999', '修業', '999999999', '999999999');
 
 -- --------------------------------------------------------
 
@@ -46,12 +54,38 @@ CREATE TABLE `edu` (
 
 CREATE TABLE `exp` (
   `id` int(20) NOT NULL,
-  `acct` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `see` tinyint(1) NOT NULL DEFAULT 1,
-  `dur` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `corp` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `posit` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `jd` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `acct` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '帳號',
+  `see` tinyint(1) NOT NULL DEFAULT 0 COMMENT '可見',
+  `dur` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '任職期間',
+  `corp` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '公司',
+  `posit` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '職稱',
+  `jd` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '工作說明'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `exp`
+--
+
+INSERT INTO `exp` (`id`, `acct`, `see`, `dur`, `corp`, `posit`, `jd`) VALUES
+(2, 'ic123456', 1, '22222222222', '3333333333', '22222222222', '33333333333'),
+(3, 'ic123456', 0, '55555555555', '55555555555', '55555555555', '55555555555');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `reqs`
+--
+
+CREATE TABLE `reqs` (
+  `id` int(11) NOT NULL,
+  `acct` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '帳號',
+  `see` tinyint(1) NOT NULL DEFAULT 0 COMMENT '可見',
+  `reqs_posit` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '期望職務',
+  `reqs_jd` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '工作描述',
+  `reqs_time` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '可上班時間',
+  `reqs_type` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '期望工作性質',
+  `reqs_place` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '期望工作地點',
+  `reqs_pay` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '期望薪資'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -61,13 +95,22 @@ CREATE TABLE `exp` (
 --
 
 CREATE TABLE `skill` (
-  `id` int(20) NOT NULL,
+  `id` int(11) NOT NULL,
   `acct` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '帳號',
-  `see` tinyint(1) NOT NULL DEFAULT 1 COMMENT '可見',
+  `see` tinyint(1) NOT NULL DEFAULT 0 COMMENT '可見',
   `cat` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '分類',
   `skill` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '技能',
   `level` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '程度'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `skill`
+--
+
+INSERT INTO `skill` (`id`, `acct`, `see`, `cat`, `skill`, `level`) VALUES
+(1, 'ic123456', 1, '管理類', '22222222222222', '7777777777777'),
+(2, 'ic123456', 1, '管理類', '3333333333333333333333', '3333333333333333333333'),
+(4, 'ic123456', 1, '美容美髮', '88888888888888', '88888888888888');
 
 -- --------------------------------------------------------
 
@@ -76,9 +119,9 @@ CREATE TABLE `skill` (
 --
 
 CREATE TABLE `social_m` (
-  `id` int(20) NOT NULL,
+  `id` int(11) NOT NULL,
   `acct` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '帳號',
-  `see` tinyint(1) NOT NULL DEFAULT 1 COMMENT '可見',
+  `see` tinyint(1) NOT NULL DEFAULT 0 COMMENT '可見',
   `fb` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `ig` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `linkedin` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -87,6 +130,15 @@ CREATE TABLE `social_m` (
   `twitter` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- 傾印資料表的資料 `social_m`
+--
+
+INSERT INTO `social_m` (`id`, `acct`, `see`, `fb`, `ig`, `linkedin`, `github`, `youtube`, `twitter`) VALUES
+(1, 'ic123456', 0, '111111111111', '111111111111', '111111111111', '111111111111', '111111111111', '111111111111'),
+(2, 'ic123456', 0, '22222222222', '22222222222', '22222222222', '22222222222', '22222222222', '22222222222'),
+(3, 'ic123456', 0, '77777777777', '77777777777', '77777777777', '77777777777', '77777777777', '77777777777');
+
 -- --------------------------------------------------------
 
 --
@@ -94,11 +146,19 @@ CREATE TABLE `social_m` (
 --
 
 CREATE TABLE `s_intro` (
-  `id` int(20) NOT NULL,
+  `id` int(11) NOT NULL,
   `acct` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '帳號',
-  `see` tinyint(1) NOT NULL DEFAULT 1 COMMENT '可見',
+  `see` tinyint(1) NOT NULL DEFAULT 0 COMMENT '可見',
   `s_intro` varchar(2000) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '自介'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `s_intro`
+--
+
+INSERT INTO `s_intro` (`id`, `acct`, `see`, `s_intro`) VALUES
+(2, 'ic123456', 0, '5555555555555555\r\n5555555555555555\r\n5555555555555555\r\n5555555555555555\r\n5555555555555555\r\n5555555555555555'),
+(4, 'ic123456', 1, '88888888888\n\n\n88888888888\n\n\n88888888888');
 
 -- --------------------------------------------------------
 
@@ -107,7 +167,7 @@ CREATE TABLE `s_intro` (
 --
 
 CREATE TABLE `user` (
-  `id` int(20) NOT NULL,
+  `id` int(11) NOT NULL,
   `acct` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '帳號',
   `psw` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密碼',
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '姓名',
@@ -126,7 +186,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `acct`, `psw`, `name`, `gender`, `btd`, `email`, `tel_cell`, `tel_home`, `addr`, `reg_time`, `upt_time`) VALUES
-(1, 'ic123456', '123456', '一二三', '女性', '2019-11-06', 'irma_chen79@hotmail.com', '0900-123-456', '(03)4646-4648', '1324', '2019-11-25 05:54:45', '2019-11-25 05:54:45');
+(1, 'ic123456', '123456', '一二三', '女性', '2019-11-06', 'irma_chen79@hotmail.com', '0900-123-456', '(03)4646-4648', '桃園', '2019-11-25 05:54:45', '2019-12-05 02:04:44');
 
 --
 -- 已傾印資料表的索引
@@ -142,6 +202,12 @@ ALTER TABLE `edu`
 -- 資料表索引 `exp`
 --
 ALTER TABLE `exp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `reqs`
+--
+ALTER TABLE `reqs`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -176,37 +242,43 @@ ALTER TABLE `user`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `edu`
 --
 ALTER TABLE `edu`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `exp`
 --
 ALTER TABLE `exp`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `reqs`
+--
+ALTER TABLE `reqs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `skill`
 --
 ALTER TABLE `skill`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `social_m`
 --
 ALTER TABLE `social_m`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `s_intro`
 --
 ALTER TABLE `s_intro`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
